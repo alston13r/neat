46df89ef94ef63be59a0a767fe95a0f01a474d3d
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-23 19:38:06 -0700

    [Implement][Debug] Neat
    Added speciation toggling functionality
    Removed old debugging variable
    Removed unneccessary constructor
    Renamed step1 and step2 methods from Population
    Renamed generation method from Population

--- Brain/Neat.js
+++ Brain/Neat.js
@@ -1,5 +1,3 @@
-let tempPop
-
 class NeatSolutionValues {
   static XOR = new NeatSolutionValues([
     {
@@ -92,13 +90,6 @@ class NeatSolutionValues {
 class Neat {
   /**
    * @param {Graphics} graphics 
-   */
-  constructor(graphics) {
-    this.graphics = graphics
-  }
-
-  /**
-   * @param {Graphics} graphics 
    * @returns {Neat}
    */
   setGraphics(graphics) {
@@ -128,19 +119,19 @@ class Neat {
     // steering
     // fire
     let population = new Population(50, 7, 0, 3, 1)
-    population.generation()
+    population.calculateFitness()
     let fragments = this.fragmentGame(Game, population)
     this.runThroughFragments(fragments)
-    population.step2()
+    population.speciate()
 
     while (population.generationCounter < 1000) {
-      population.step1()
-      population.generation()
+      population.nextGeneration()
+      population.calculateFitness()
       fragments = this.fragmentGame(Game, population)
       this.runThroughFragments(fragments)
       let fittest = population.getFittest()
       // fittest.draw(this.graphics)
-      population.step2()
+      population.speciate()
     }
 
 
@@ -179,19 +170,21 @@ class Neat {
         brain.runTheNetwork()
         let o = brain.getOutput()
         let d = v.outputs.map((exp, i) => Math.abs(exp - o[i]))
-        brain.fitness -= d.reduce((sum, curr) => sum + curr, 0)
+        brain.fitness -= d.reduce((sum, curr) => sum + curr)
       }
     }
 
     let population = new Population(1000, values.values[0].inputs.length, 0, values.values[0].outputs.length, 1)
       .setGraphics(this.graphics)
-    population.generation(fitnessFunction)
-    population.step2()
+    population.calculateFitness(fitnessFunction)
+    population.speciate()
 
     return new Promise((resolve) => {
       let iterate = () => {
-        this.graphics.bg()
-        population.draw()
+        if (this.graphics) {
+          this.graphics.bg()
+          population.draw()
+        }
         if (population.fittestEver == null || population.fittestEver.fitness >= desiredFitness) {
           let solution = population.fittestEver
           console.log(`Solution found`)
@@ -209,9 +202,9 @@ class Neat {
           console.log(`[1, 1] -> [${solution.getOutput().join(', ')}]`)
           return resolve(solution)
         } else {
-          population.step1()
-          population.generation(fitnessFunction)
-          population.step2()
+          population.nextGeneration()
+          population.calculateFitness(fitnessFunction)
+          if (Population.Speciation) population.speciate()
 
           setTimeout(iterate, updateInterval)
         }
