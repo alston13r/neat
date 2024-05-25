3cce69982853d1b5dd8f4cdff4b0b5fce28b2f5c
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-25 15:16:04 -0700

    [Debug] Speciation
    Removed debugging interval

--- index.js
+++ index.js
@@ -83,6 +83,6 @@ const graphics = new Graphics().setSize(800, 600).appendTo(document.body)
 
 const desired = NeatSolutionValues.XOR
 const neat = new Neat().setGraphics(graphics)
-neat.findSolution(desired, desired.maxLinearFitnessValue() - 0.05, 500).then(solution => {
+neat.findSolution(desired, desired.maxLinearFitnessValue() - 0.05).then(solution => {
   solution.draw(graphics)
 })
