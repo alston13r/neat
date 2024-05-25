ebbca6fabd56db71758efb618d5f76215099e6f2
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-25 14:50:30 -0700

    [JSDoc] NeatNode
    Modified documentation to have lowercase values for class names
    to maintain consistency with new files

--- src/Brain/NeatNode.ts
+++ src/Brain/NeatNode.ts
@@ -1,6 +1,6 @@
 /**
  * Wrapper class containing references to an assortment of activation functions.
- * An activation function normalizes a Node's output value before proceeding to
+ * An activation function normalizes a node's output value before proceeding to
  * the next layer. Activation functions are needed since neural networks are meant
  * to run on arbitrary data inputs. They serve to put data onto the same scale, since
  * $100 is hard to compare with 5 lemons, but 0s and 1s are much more comparable.
@@ -25,7 +25,7 @@ class ActivationFunction {
 
   /**
    * A static array containing references to all activation functions, this is to help
-   * with the mutation of a Node's activation function
+   * with the mutation of a node's activation function
    */
   static Arr: ActivationFunction[] = [
     ActivationFunction.Sigmoid,
@@ -55,17 +55,17 @@ class ActivationFunction {
 }
 
 /**
- * An enumerator of the types of Node's a Brain can have. An input Node serves as the data input points
+ * An enumerator of the types of node's a Brain can have. An input node serves as the data input points
  * in a Brain, they don't have activation functions (identity function) since they are meant to just
- * pass inputs into the Brain's topology. An output Node is the Brain's physical output, this is what
- * we access to interpret what the Brain thinks is the output. Hidden Nodes are everything in between
+ * pass inputs into the Brain's topology. An output node is the brain's physical output, this is what
+ * we access to interpret what the Brain thinks is the output. Hidden nodes are everything in between
  * input and output nodes, they provide the majority of the complexity of the Brain.
  */
 enum NNodeType { Input, Hidden, Output }
 
 /**
- * The Brain Node class. Node's serve as connection points within a Brain where data passes through them,
- * being activated, before continuing on to the next layer in the Brain. This is named NNode since Node
+ * The brain's node class. Nodes serve as connection points within a brain where data passes through them,
+ * being activated, before continuing on to the next layer in the brain. This is named NNode since Node
  * was already taken.
  */
 class NNode {
@@ -80,34 +80,34 @@ class NNode {
   /** The chance for the bias to be nudged rather than randomized when mutated */
   static NudgeBiasChance: number = 0.9
 
-  /** The Node's unique numerical identifier within the Brain */
+  /** The node's unique numerical identifier within the Brain */
   id: number
-  /** The Node's type, can be Input, Hidden, or Output */
+  /** The node's type, can be Input, Hidden, or Output */
   type: NNodeType
-  /** The Node's layer within the Brain's topology */
+  /** The node's layer within the Brain's topology */
   layer: number
-  /** The weighted sum of the Node's incoming connection values */
+  /** The weighted sum of the node's incoming connection values */
   sumInput: number = 0
   /** The activated sum input */
   sumOutput: number = 0
-  /** The Node's bias weight, this gets added in before activation but is not represented in the sum input value */
+  /** The node's bias weight, this gets added in before activation but is not represented in the sum input value */
   bias: number = Connection.GenerateRandomWeight()
   /** An array of incoming connections */
   connectionsIn: Connection[] = []
   /** An array of outgoing connections */
   connectionsOut: Connection[] = []
-  /** The activation function for this Node */
+  /** The activation function for this node */
   activationFunction: ActivationFunction
 
   /**
-   * Constructs a Brain Node with the specified id, Node type, and layer. The id
+   * Constructs a brain node with the specified id, node type, and layer. The id
    * is used as an identifier for connection innovation ids. Node ids are generated
-   * automatically within the Brain's constructor. The type specifies the type of Node,
+   * automatically within the Brain's constructor. The type specifies the type of node,
    * options are Input, Hidden, and Output. The layer specifies the layer within the
    * Brain's topology, this indicates how input data propagates throughout the Brain. 
-   * @param id the Node's numerical identifier
-   * @param type the Node's type
-   * @param layer the Node's layer
+   * @param id the node's numerical identifier
+   * @param type the node's type
+   * @param layer the node's layer
    */
   constructor(id: number, type: NNodeType, layer: number) {
     this.id = id
@@ -117,7 +117,7 @@ class NNode {
   }
 
   /**
-   * Activates the weighted sum of input values for this Node. This adds the bias node before activation and
+   * Activates the weighted sum of input values for this node. This adds the bias node before activation and
    * sets the sum output value to whatever the activation function returns.
    */
   activate(): void {
@@ -125,7 +125,7 @@ class NNode {
   }
 
   /**
-   * Clones this Node and returns said clone with the same id, type, layer, bias weight, and activation function.
+   * Clones this node and returns said clone with the same id, type, layer, bias weight, and activation function.
    * @returns the clone
    */
   clone(): NNode {
@@ -136,9 +136,9 @@ class NNode {
   }
 
   /**
-   * Mutates the bias weight and, if allowed for the Node's type, the activation function.
+   * Mutates the bias weight and, if allowed for the node's type, the activation function.
    * Mutations occur by chance, only if a call to Math.random() yields a value less than
-   * the predefined static values. A Node's bias, when mutated, can either be nudged or
+   * the predefined static values. A node's bias, when mutated, can either be nudged or
    * completely randomized.
    */
   mutate(): void {
