445303037f451c5599b726da5f90b4c7194e0eb7
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-26 04:58:37 -0700

    [Refactor] NeuralNetwork, Maths
    Moved the mutateWeight function from Maths to here since that seems
    more appropriate

--- src/NeuralNetwork/NeuralNetwork.ts
+++ src/NeuralNetwork/NeuralNetwork.ts
@@ -1,3 +1,8 @@
+// function mutateWeight(x: number): number {
+//   if (Math.random() < 0.05) return x + gauss() * 0.5
+//   return x
+// }
+
 // class BasicNeuralNetwork {
 //   inputSize: number
 //   outputSize: number
