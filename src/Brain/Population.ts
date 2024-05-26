094090831d956f14d2d9f7bf58184c7bb0789e96
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-25 21:00:17 -0700

    [JSDoc] Brain, Population
    Added documentation to Population and Brain

--- src/Brain/Population.ts
+++ src/Brain/Population.ts
@@ -1,28 +1,46 @@
 // @ts-nocheck
 /**
- * TODO
+ * A population is a collection of a bunch of brains and allows for a large amount
+ * of brains to explore different topologies. Larger populations allow for solutions
+ * to be found quciker. This class also houses the methods necessary to produce the
+ * offspring for the next generation.
  */
 class Population {
+  /** Toggle for speciation between generations */
   static Speciation: boolean = true
+  /**
+   * Toggle for elitism, where the specified percent of members gets preserved from
+   * each generation with no mutations
+   */
   static Elitism: boolean = true
+  /** The percent of members who get carried over as elites */
   static ElitePercent: number = 0.3
 
+  /** A counter for the current generation */
   generationCounter: number = 0
+  /** An array of the population's members */
   members: Brain[] = []
+  /** The number of members the population will always have */
   popSize: number
+  /** The number of input nodes that members will be initialized with */
   inputN: number
+  /** The number of hidden nodes that members will be initialized with */
   hiddenN: number
+  /** The number of output nodes that members will be initialized with */
   outputN: number
+  /** The enabled connections percent that members will be initialized with */
   enabledChance: number
+  /** A reference to the graphics object that the population can be drawn to */
   graphics: Graphics
 
   /**
-   * TODO
-   * @param popSize 
-   * @param inputN 
-   * @param hiddenN 
-   * @param outputN 
-   * @param enabledChance 
+   * Constructs a population with the specified size, input nodes, hidden nodes, output nodes,
+   * and chance for connections to start enabled.
+   * @param popSize the population size
+   * @param inputN the number of input nodes
+   * @param hiddenN the number of hidden nodes
+   * @param outputN the number of output nodes
+   * @param enabledChance the chance for connections to start enabled
    */
   constructor(popSize: number, inputN: number, hiddenN: number, outputN: number, enabledChance: number = 1) {
     this.popSize = popSize
@@ -33,15 +51,7 @@ class Population {
   }
 
   /**
-   * TODO
-   */
-  setGraphics(graphics: Graphics): Brain {
-    this.graphics = graphics
-    return this
-  }
-
-  /**
-   * TODO
+   * The list of all current species that the members are registered to.
    */
   get speciesList(): Species[] {
     const returnArr: Species[] = []
@@ -52,17 +62,20 @@ class Population {
   }
 
   /**
-   * TODO
+   * Adjusts the dynamic compatibility threshold for the species class given the current
+   * number of species and the desired amount. If speciation is disabled, this will not be called.
    */
   adjustThreshold(): void {
     Species.DynamicThreshold += Math.sign(this.speciesList.length - Species.TargetSpecies) * Species.DynamicThresholdStepSize
   }
 
-  // /**
-  //  * @param {function(Brain): void} fitnessFunction 
-  //  * @returns {Brain | null}
-  //  */
-  calculateFitness(fitnessFunction) {
+  /**
+   * Runs the specified fitness function on the population's members. This keeps track
+   * of the fittest member of this iteration as well as the fittest member ever.
+   * @param fitnessFunction the fitness function
+   * @returns the fittest member of the iteration
+   */
+  calculateFitness(fitnessFunction: (brain: Brain) => number) {
     for (let i = this.members.length; i < this.popSize; i++) {
       this.members.push(new Brain().initialize(this.inputN, this.hiddenN, this.outputN, this.connPerc))
     }
@@ -76,36 +89,38 @@ class Population {
       if (this.fittestEver == null || fittest.fitness > this.fittestEver.fitness) this.fittestEver = fittest
     }
 
-    this.fittest = this.members.reduce((best, curr) => curr.fitness > best.fitness ? curr : best)
-
-    return fittest
+    return this.getFittest()
   }
 
   /**
-   * TODO
-   * @returns 
+   * Returns the fittest member in the current list of members.
+   * @returns the fittest member
    */
   getFittest(): Brain {
     return this.members.reduce((best, curr) => curr.fitness > best.fitness ? curr : best)
   }
 
   /**
-   * TODO
+   * Updates the gensSinceImproved counter for each species given
+   * whether or not they have improved their fitness since the last generation.
+   * If speciation is disabled, this will not be called.
    */
   updateGensSinceImproved(): void {
     this.speciesList.forEach(species => species.updateGensSinceImproved())
   }
 
   /**
-   * TODO
+   * Goes through each species and calls their adjustFitness() methods. Adjusting
+   * the fitness will normalize each member's fitness to their individual species.
+   * If speciation is disabled, this will not be called.
    */
   adjustFitness(): void {
     this.speciesList.forEach(species => species.adjustFitness())
   }
 
   /**
-   * TODO
-   * @returns 
+   * Returns the average fitness of all the members in the population.
+   * @returns the average fitness of all members
    */
   getAverageFitness(): number {
     const N: number = this.members.length
@@ -113,8 +128,8 @@ class Population {
   }
 
   /**
-   * 
-   * @returns 
+   * Returns the average adjusted fitness of all the members in the population.
+   * @returns the average adjusted fitness of all members
    */
   getAverageFitnessAdjusted(): number {
     const N: number = this.members.length
@@ -122,7 +137,9 @@ class Population {
   }
 
   /**
-   * TODO
+   * Calculates the number of allowed offspring that each species can produce.
+   * If speciation is disabled, this calculation will not run and offspring are
+   * produced solely by the fittest members of the population.
    */
   calculateAllowedOffspring(): void {
     const max: number = this.popSize
@@ -147,7 +164,11 @@ class Population {
   }
 
   /**
-   * TODO
+   * Produces the next generation of members. If speciation is enabled, it produces them
+   * based on the calculation done in the calculateAllowedOffspring() method. If speciation
+   * is disabled, members are simply produced based on the fittest members. Elitism will
+   * preserve the specified percentage of members in each species when speciation is enabled,
+   * otherwise its the percentage of members that gets preserved.
    */
   produceOffspring(): void {
     if (Population.Speciation) {
@@ -171,14 +192,16 @@ class Population {
   }
 
   /**
-   * TODO
+   * Goes through all the members in the population and calls their mutate() method.
    */
   mutate(): void {
     this.members.forEach(member => member.mutate())
   }
 
   /**
-   * TODO
+   * Produces the next generation of members for the population. This produces the offspring,
+   * increments the generationCounter denoting which generation the current members are, and
+   * mutates them.
    */
   nextGeneration(): void {
     this.produceOffspring()
@@ -187,7 +210,10 @@ class Population {
   }
 
   /**
-   * TODO
+   * Speciates the current members of the population. If speciation is disabled, this is
+   * completely skipped during the evolution process. This speciates members, updates
+   * each species gensSinceImproved counters, adjusts the dynamic compatibility threshold,
+   * adjusts the fitness of all members, and calculates the allowed offspring for each species.
    */
   speciate(): void {
     Species.Speciate(this)
@@ -198,10 +224,12 @@ class Population {
   }
 
   /**
-   * TODO
-   * @param list 
-   * @param offspringN 
-   * @returns 
+   * Static helper method to generate pairings of brains that will serve as parents
+   * for the next generation. Parents are chosen based on fitness and rolled through
+   * a roulette wheel.
+   * @param list the list of parents to choose from
+   * @param offspringN the number of offspring desired
+   * @returns an array of parent pairings where parents are p1 and p2
    */
   static GeneratePairings(list: Brain[], offspringN: number): { p1: Brain, p2: Brain }[] {
     if (offspringN == 0) return []
@@ -212,10 +240,10 @@ class Population {
   }
 
   /**
-   * TODO
-   * @param list 
-   * @param softLimit 
-   * @returns 
+   * Static helper method to produce an array of elites.
+   * @param list the list of members to select elites from
+   * @param softLimit the soft limit for the number of elites
+   * @returns the elites
    */
   static GetElites(list: Brain[], softLimit: number): Brain[] {
     if (softLimit == 0) return []
@@ -230,7 +258,20 @@ class Population {
     return res
   }
 
-  draw() {
+  /**
+   * Sets the local reference for graphics to the specified object.
+   * @param graphics the graphics to set
+   * @returns a refrence to this population
+   */
+  setGraphics(graphics: Graphics): Population {
+    this.graphics = graphics
+    return this
+  }
+
+  /**
+   * Draws this population to the local graphics.
+   */
+  draw(): void {
     const round = (x, p) => Math.round(x * 10 ** p) / 10 ** p
 
     this.graphics.bg()
