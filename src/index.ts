d1972cc33ac4fcaf2077d90704a6775c0a60098c
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-28 06:03:08 -0700

    [Cleanup] index
    Removed old commented code

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,4 @@
-// const desired = NeatSolutionValues.XOR
-// const fitnessFunction = brain => {
-//   brain.fitness = desired.maxLinearFitnessValue()
-//   for (let v of desired.random()) {
-//     brain.loadInputs(v.inputs)
-//     brain.runTheNetwork()
-//     let o = brain.getOutput()
-//     let d = v.outputs.map((exp, i) => Math.abs(exp - o[i]))
-//     brain.fitness -= d.reduce((sum, curr) => sum + curr, 0)
-//   }
-// }
-
 const graphics = new Graphics().setSize(800, 600).appendTo(document.body)
-// const population = new Population(1000, 2, 0, 1, 1)
-// population.generation(fitnessFunction)
-// population.step2()
-// population.draw(graphics)
-
-
-// // let game = new Game(graphics)
-// // // // let neat = new Neat(graphics)
-// // // // neat.runOnGame(Game)
-
 // // game.addEventListener(GameEvent.ShipDied, () => {
 // //   console.log('You died')
 // //   wantsToStop = true
@@ -58,30 +36,7 @@ const graphics = new Graphics().setSize(800, 600).appendTo(document.body)
 
 
 
-
-
-
-
-
-
-// // GENERATION ZERO
-// // create population
-
-// // GENERATION X
-// // produce offspring
-// // incremement generation counter
-// // mutate population
-
-// // OVERLAP
-// // fill in any missing spots
-// // calculate fitness
-// // speciation
-// // gens since improved
-// // speciation threshold
-// // adjust fitness
-// // calculate allowed offspring
-
-const desired = NeatSolutionValues.XOR
+const desired = TrainingValues.XOR
 const neat = new Neat().setGraphics(graphics)
 neat.findSolution(desired, desired.maxLinearFitnessValue() - 0.05).then(solution => {
   graphics.bg()
