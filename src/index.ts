1583c2365818a03915cdcefde27ef1eb5eeb5dd8
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-29 09:52:24 -0700

    [Debug][Test] Asteroids
    Test code for the Asteroids game

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,43 @@
 const graphics = new Graphics().setSize(800, 600).appendTo(document.body)
-const desired = TrainingValues.XOR
-const neat = new Neat().setGraphics(graphics)
-neat.findSolution(desired, desired.maxLinearFitnessValue() - 0.05).then(solution => {
-  graphics.bg()
-  solution.setGraphics(graphics).draw()
+// const desired = TrainingValues.XOR
+// const neat = new Neat().setGraphics(graphics)
+// neat.findSolution(desired, desired.maxLinearFitnessValue() - 0.05).then(solution => {
+//   graphics.bg()
+//   solution.setGraphics(graphics).draw()
+// })
+
+
+let wantsToStop: boolean = false
+const game = new AsteroidsGame(graphics)
+game.addEventListener(ShipEvent.ShipDied, () => {
+  console.log('You died')
+  wantsToStop = true
+  console.log(game.asteroidCounter, game.frameCounter)
 })
 
 
 
-// const game = new Game()
-// game.addEventListener(GameEvent.ShipDied, () => {
-//   console.log('You died')
-//   wantsToStop = true
-//   console.log(game.asteroidCounter, game.frameCounter)
-// })
+const keysPressed = {}
+window.addEventListener('keydown', e => {
+  keysPressed[e.key] = true
+})
+window.addEventListener('keyup', e => {
+  keysPressed[e.key] = false
+})
 
+function gameLoop() {
+  graphics.bg()
 
+  game.loadInputs(keysPressed)
 
-// let keysPressed = {}
-// window.addEventListener('keydown', e => {
-//   keysPressed[e.key] = true
-// })
-// window.addEventListener('keyup', e => {
-//   keysPressed[e.key] = false
-// })
+  game.update()
+  game.draw()
 
-// let wantsToStop = false
+  if (!wantsToStop) lastFrame = window.requestAnimationFrame(gameLoop)
+}
 
-// function gameLoop() {
-//   graphics.bg()
+let lastFrame: number = window.requestAnimationFrame(gameLoop)
 
-//   game.loadInputs(keysPressed)
-
-//   game.update()
-//   game.draw()
-
-//   if (!wantsToStop) lastFrame = window.requestAnimationFrame(gameLoop)
-// }
-
-// let lastFrame = window.requestAnimationFrame(gameLoop)
-
-// function stopGameLoop() {
-//   wantsToStop = true
-// }
+function stopGameLoop() {
+  wantsToStop = true
+}
