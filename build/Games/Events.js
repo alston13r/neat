d841e23681dc31c578f790e8dc5953ef2692cd2e
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-29 10:07:21 -0700

    [Build]

--- build/Games/Events.js
+++ build/Games/Events.js
@@ -3,7 +3,7 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _a, _GameEvent_Start, _GameEvent_End, _GameEvent_FrameUpdate, _GameEvent_GraphicsSet;
+var _a, _GameEvent_Start, _GameEvent_End, _GameEvent_FrameUpdate;
 class GameEvent {
     constructor(type) {
         this.symbol = Symbol(type);
@@ -11,11 +11,9 @@ class GameEvent {
     static get Start() { return __classPrivateFieldGet(_a, _a, "f", _GameEvent_Start); }
     static get End() { return __classPrivateFieldGet(_a, _a, "f", _GameEvent_End); }
     static get FrameUpdate() { return __classPrivateFieldGet(_a, _a, "f", _GameEvent_FrameUpdate); }
-    static get GraphicsSet() { return __classPrivateFieldGet(_a, _a, "f", _GameEvent_GraphicsSet); }
 }
 _a = GameEvent;
 _GameEvent_Start = { value: new _a('start') };
 _GameEvent_End = { value: new _a('end') };
 _GameEvent_FrameUpdate = { value: new _a('frame update') };
-_GameEvent_GraphicsSet = { value: new _a('graphics set') };
 //# sourceMappingURL=Events.js.map
