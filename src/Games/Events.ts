7b8289f3bc3a3b84367291786896cba832b07f8e
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-29 09:54:58 -0700

    [Cleanup] Game Events
    Removed unnecessary GraphicsSet event

--- src/Games/Events.ts
+++ src/Games/Events.ts
@@ -2,7 +2,6 @@ class GameEvent {
   static #Start: GameEvent = new GameEvent('start')
   static #End: GameEvent = new GameEvent('end')
   static #FrameUpdate: GameEvent = new GameEvent('frame update')
-  static #GraphicsSet: GameEvent = new GameEvent('graphics set')
 
   symbol: Symbol
 
@@ -13,5 +12,4 @@ class GameEvent {
   static get Start(): GameEvent { return GameEvent.#Start }
   static get End(): GameEvent { return GameEvent.#End }
   static get FrameUpdate(): GameEvent { return GameEvent.#FrameUpdate }
-  static get GraphicsSet(): GameEvent { return GameEvent.#GraphicsSet }
 }
