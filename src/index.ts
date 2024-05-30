c56b962340e74e9977250a88461f2f0aed816ed0
Alston Rodriguez <avrodri3@ncsu.edu>
2024-05-29 18:50:41 -0700

    [Implement] Neat
    Added some test code for the neat algorithnm running
    on Asteroids

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,99 @@
 const graphics = new Graphics().setSize(800, 600).appendTo(document.body)
-// const desired = TrainingValues.XOR
-// const neat = new Neat().setGraphics(graphics)
-// neat.findSolution(desired, desired.maxLinearFitnessValue() - 0.05).then(solution => {
-//   graphics.bg()
-//   solution.setGraphics(graphics).draw()
-// })
 
+const maxTimeAlive: number = 30
+let generationTimeAlive: number = 0
 
-let wantsToStop: boolean = false
-const game = new AsteroidsGame(graphics)
-game.addEventListener(ShipEvent.ShipDied, () => {
-  console.log('You died')
-  wantsToStop = true
-  console.log(game.asteroidCounter, game.frameCounter)
-})
+class GameBrainPair {
+  brain: Brain
+  game: AsteroidsGame
+  lastFrame: number
+  drawing: boolean = false
 
+  constructor(brain: Brain) {
+    this.brain = brain
+    this.game = new AsteroidsGame(graphics).addEventListener(ShipEvent.ShipDied, () => this.kill())
+  }
 
+  kill() {
+    this.updateFitness()
+    deadCount++
+    deadSet.push(aliveSet.splice(aliveSet.indexOf(this), 1)[0])
+    this.drawing = false
+    if (aliveSet.length > 0) aliveSet[0].drawing = true
+  }
 
-const keysPressed = {}
-window.addEventListener('keydown', e => {
-  keysPressed[e.key] = true
-})
-window.addEventListener('keyup', e => {
-  keysPressed[e.key] = false
-})
+  updateFitness() {
+    this.brain.fitness = this.game.asteroidCounter * 40 + this.game.frameCounter / 600
+  }
 
-function gameLoop() {
-  graphics.bg()
+  loop(): void {
+    if (this.drawing) graphics.bg()
 
-  game.loadInputs(keysPressed)
+    loadBrainInputs(this)
+    this.brain.runTheNetwork()
+    const brainOutputs: number[] = this.brain.getOutput()
+    this.game.ship.loadInputs(...brainOutputs)
 
-  game.update()
-  game.draw()
+    this.game.update()
+    if (this.drawing) {
+      this.game.draw()
+      this.game.graphics.createText(`Generation: ${population.generationCounter}`, 5, this.game.graphics.height - 5, '#fff', 10, 'left', 'bottom').draw()
+      this.game.graphics.createText(`Alive: ${population.popSize - deadCount} / ${population.popSize}`, 5, this.game.graphics.height - 15, '#fff', 10, 'left', 'bottom').draw()
+      this.game.graphics.createText(`Alive for: ${Math.round(generationTimeAlive)} / ${maxTimeAlive} seconds`, 5, 15, '#fff', 10, 'left', 'top').draw()
+    }
 
-  if (!wantsToStop) lastFrame = window.requestAnimationFrame(gameLoop)
+    if (this.game.ship.alive) this.lastFrame = window.requestAnimationFrame(() => this.loop())
+  }
 }
 
-let lastFrame: number = window.requestAnimationFrame(gameLoop)
-
-function stopGameLoop() {
-  wantsToStop = true
+function loadBrainInputs(pair: GameBrainPair) {
+  const inputs: number[] = []
+  const shipInfo: ShipInfo = pair.game.ship.getInfo()
+  inputs[0] = shipInfo.posX
+  inputs[1] = shipInfo.posY
+  inputs[2] = shipInfo.heading
+  inputs[3] = shipInfo.velX
+  inputs[4] = shipInfo.velY
+  inputs[5] = shipInfo.canShoot ? 1 : 0
+  const nearestAsteroidInfo: AsteroidInfo = pair.game.getAsteroidsByDistance()[0].getInfo()
+  inputs[6] = nearestAsteroidInfo.angleFromShip
+  inputs[7] = nearestAsteroidInfo.distanceFromShip
+  inputs[8] = nearestAsteroidInfo.velX
+  inputs[9] = nearestAsteroidInfo.velY
+  inputs[10] = nearestAsteroidInfo.size
+  pair.brain.loadInputs(inputs)
 }
+
+const population: Population = new Population(200, 11, 0, 3)
+const aliveSet: GameBrainPair[] = population.members.map(brain => new GameBrainPair(brain))
+const deadSet: GameBrainPair[] = []
+let deadCount: number = 0
+aliveSet[0].drawing = true
+aliveSet.forEach(pair => pair.loop())
+
+const fittestRecords: Brain[] = []
+
+let lastTimestamp: number = 0
+
+function mainLoop(timestamp: number) {
+  const diff: number = timestamp - lastTimestamp
+  lastTimestamp = timestamp
+  generationTimeAlive += diff / 1000
+
+  if (deadCount == population.popSize) {
+    generationTimeAlive = 0
+    fittestRecords.push(population.getFittest())
+    if (Population.Speciation) population.speciate()
+    population.nextGeneration()
+    deadSet.length = 0
+    population.members.forEach(brain => aliveSet.push(new GameBrainPair(brain)))
+    deadCount = 0
+    aliveSet[0].drawing = true
+    aliveSet.forEach(pair => pair.loop())
+  } else if (generationTimeAlive > maxTimeAlive) {
+    aliveSet.forEach(pair => pair.game.ship.kill())
+  }
+  window.requestAnimationFrame(mainLoop)
+}
+
+window.requestAnimationFrame(mainLoop)
