24c9567083e51656760c7a7cab9e6ed085c46cc6
Alston Rodriguez <avrodri3@ncsu.edu>
2024-06-03 05:19:42 -0700

    [Refactor] Neat, Brain
    Disabled recurrent connections
    
    Added a reference for the Brain's population
    
    Added more JSDoc for the initialize() method
    
    Updated the clone() method
      - handle population's instance flag for speciation instead of the
      static one
      - handle the new population reference

--- src/Brain/Brain.ts
+++ src/Brain/Brain.ts
@@ -11,7 +11,7 @@ class Brain {
   /** Toggle for connection disabling */
   static AllowDisablingConnections: boolean = false
   /** Toggle for allowing recurrent connections */
-  static AllowRecurrent: boolean = true
+  static AllowRecurrent: boolean = false
   /** The chance for a new connection to be made */
   static AddConnectionChance: number = 0.4
   /** The chance for a connection to be disabled */
@@ -41,6 +41,23 @@ class Brain {
   isElite: boolean = false
   /** A reference to the graphics object that the brain can be drawn to */
   graphics: Graphics
+  /** A reference to this brain's population */
+  population: Population
+
+  /**
+   * Constructs a brain. This does not initialize the brain's topology,
+   * call initialize() on the brain to do that.
+   */
+  constructor()
+  /**
+   * Constructs a brain with the specified population reference. This does not
+   * initialize the brain's topology, call initialize() on the brain to do that.
+   * @param population the population
+   */
+  constructor(population: Population)
+  constructor(population?: Population) {
+    this.population = population
+  }
 
   /**
    * Initializes the brain's topology to contain the specified number of input nodes,
@@ -48,9 +65,19 @@ class Brain {
    * @param inputN the number of input nodes
    * @param hiddenN the number of hidden nodes
    * @param outputN the number of output nodes
-   * @param enabledChance the chance for connections to start enabled, 1 (100%) by default
    * @returns a reference to this Brain
    */
+  initialize(inputN: number, hiddenN: number, outputN: number): Brain
+  /**
+   * Initializes the brain's topology to contain the specified number of input nodes,
+   * hidden nodes, output nodes, and enabled chance.
+   * @param inputN the number of input nodes
+   * @param hiddenN the number of hidden nodes
+   * @param outputN the number of output nodes
+   * @param enabledChance the chance for connections to start enabled
+   * @returns a reference to this Brain
+   */
+  initialize(inputN: number, hiddenN: number, outputN: number, enabledChance: number): Brain
   initialize(inputN: number, hiddenN: number, outputN: number, enabledChance: number = 1): Brain {
     this.nodes = []
     this.inputNodes = []
@@ -304,10 +331,10 @@ class Brain {
    * @returns the clone
    */
   clone(): Brain {
-    const clone = new Brain()
+    const clone = new Brain(this.population)
 
     // species
-    if (Population.Speciation) {
+    if (this.population.speciation) {
       clone.species = this.species
       clone.species.members.push(clone)
     }
