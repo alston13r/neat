c3a0f22f0e1311747a71e5cd3e1c48725ad1b364
Alston Rodriguez <avrodri3@ncsu.edu>
2024-06-03 05:37:13 -0700

    [Refactor] Neat, TrainingValues
    Added a type for ease-of-use when referencing an input-output object
    
    Added values for inputSize and outputSize to assist with fitness
    calculations and brain constructing
      - inputSize refers to the size of the training values input values
      - outputSize refers to the output values
      - these are updated when addValue() is called in case they weren't
      set in the constructor
    
    Modified constructor parameter flags and JSDoc
    
    Added a length getter which returns the length of the internal values
    array
    
    Converted the ordered() and random() generator methods to be getters
    that simply return a shallow copy of the internal values array, be it
    ordered or shuffled
    
    Removed unnecessary maxLinearFitnessValue() calculation and method

--- src/Utils/TrainingValues.ts
+++ src/Utils/TrainingValues.ts
@@ -1,4 +1,14 @@
 /**
+ * Type outlining what an entry in the TrainingValues class needs to have.
+ * This is mostly for ease-of-use when writing types since writing out
+ * objects is a pain.
+ */
+type InputOutput = {
+  inputs: number[]
+  outputs: number[]
+}
+
+/**
  * Utility class to help with organizing basic training data. This already includes data
  * for XOR, OR, and AND training.
  */
@@ -62,14 +72,27 @@ class TrainingValues {
   ])
 
   values: { inputs: number[], outputs: number[] }[]
+  inputSize: number = 0
+  outputSize: number = 0
 
   /**
+   * Constructs a TrainingValues wrapper object. Values can be added using addValue(),
+   * which return a reference to this object to allow for chaining.
+   */
+  constructor()
+  /**
    * Constructs a TrainingValues wrapper object for the specified values. If the
-   * values are null, then the values are initialized to an empty array.
+   * values are null, then the values are initialized to an empty array. Values can be
+   * added using addValue(), which return a reference to this object to allow for chaining.
    * @param values the values
    */
-  constructor(values: { inputs: number[], outputs: number[] }[] | null) {
+  constructor(values: { inputs: number[], outputs: number[] }[])
+  constructor(values?: { inputs: number[], outputs: number[] }[]) {
     this.values = values || []
+    if (values != null) {
+      this.inputSize = this.values[0].inputs.length
+      this.outputSize = this.values[0].outputs.length
+    }
   }
 
   /**
@@ -79,36 +102,37 @@ class TrainingValues {
    * @returns a reference to this TrainingValues object
    */
   addValue(inputs: number[], outputs: number[]): TrainingValues {
+    if (this.inputSize == 0 || this.outputSize == 0) {
+      this.inputSize = inputs.length
+      this.outputSize = outputs.length
+    }
     this.values.push({ inputs, outputs })
     return this
   }
 
+  /** The number of training values in this set */
+  get length(): number {
+    return this.values.length
+  }
+
   /**
-   * Returns a generator for the local values in an ordered sequence.
-   * @returns the values as a generator
+   * Returns a shalloy copy of the input output values in an ordered sequence.
+   * @returns the array values
    */
-  *ordered(): Generator<{ inputs: number[], outputs: number[] }, void, undefined> {
-    yield* this.values
+  get ordered(): InputOutput[] {
+    return [...this.values]
   }
 
   /**
    * Returns a generator for the local values in a random sequence.
-   * @returns the values as a generator
+   * @returns the array of values
    */
-  *random(): Generator<{ inputs: number[], outputs: number[] }, void, undefined> {
-    let t = [...this.values]
-    let m = t.length
-    for (let i = 0; i < m; i++) {
-      yield t.splice(Math.floor(Math.random() * t.length), 1)[0]
+  get random(): InputOutput[] {
+    const temp: InputOutput[] = [...this.values]
+    const res: InputOutput[] = []
+    while (temp.length > 0) {
+      res.push(temp.splice(Math.floor(Math.random() * temp.length), 1)[0])
     }
-  }
-
-  /**
-   * Returns the max linear fitness available by these values, simply the number of
-   * values multiplies by the size of the output.
-   * @returns the max linear fitness
-   */
-  maxLinearFitnessValue() {
-    return this.values.length * this.values[0].outputs.length
+    return res
   }
 }
