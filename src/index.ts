4c9e344edc18a74cd8ca35e1750a857ec8b752fc
Alston Rodriguez <avrodri3@ncsu.edu>
2024-06-03 21:32:19 -0700

    [Refactor][Implement] Neat, Asteroids
    Added functionality for Population

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 const gameGraphics: Graphics = new Graphics().setSize(800, 600).appendTo(document.body)
+const population: Population = new Population(500, 11, 0, 3, 0.5)
 
 const game: AsteroidsGame = new AsteroidsGame(gameGraphics)
 const brain: Brain = new Brain().initialize(11, 0, 3, 0.5)
 
-function thinkBrain(): number[] {
+const maxTimeAlive: number = 30
+let currentGenerationTimeAlive: number = 0
+
+type GameBrainPair = {
+  game: AsteroidsGame
+  brain: Brain
+}
+
+function thinkBrain(brain: Brain, game: AsteroidsGame): number[] {
   const inputs: number[] = []
   const shipInfo: ShipInfo = game.ship.getInfo()
   inputs[0] = shipInfo.posX
@@ -21,24 +30,65 @@ function thinkBrain(): number[] {
   return brain.think(inputs)
 }
 
-game.addEventListener(AsteroidEvent.AsteroidDestroyed, updateFitness)
-game.addEventListener(GameEvent.End, updateFitness)
-
-function updateFitness() {
-  brain.fitness = game.asteroidCounter * 5 + game.frameCounter / 120
+function updateFitness(pair: GameBrainPair) {
+  pair.brain.fitness = pair.game.asteroidCounter * 5 + pair.game.frameCounter / 120
 }
 
-function loop() {
-  gameGraphics.bg()
-
-  const brainThoughts: number[] = thinkBrain()
-  game.ship.loadInputs(...brainThoughts)
-
-  game.update()
-  game.draw()
-
-  if (game.ship.alive) window.requestAnimationFrame(loop)
-  else console.log(`Brain has died and achieved a fitness of: ${brain.fitness}`)
+function addListeners(arr: GameBrainPair[]) {
+  arr.forEach(pair => {
+    pair.game.addEventListener(AsteroidEvent.AsteroidDestroyed, () => updateFitness(pair))
+    pair.game.addEventListener(GameEvent.End, () => updateFitness(pair))
+  })
 }
 
-loop()
+const fittestRecords: Brain[] = []
+
+let pairings: GameBrainPair[] = []
+
+let lastTimestamp: number = 0
+function loop(timestamp: number) {
+  const diff: number = timestamp - lastTimestamp
+  lastTimestamp = timestamp
+  currentGenerationTimeAlive += diff / 1000
+
+  const stillAlive: GameBrainPair[] = pairings.filter(pair => pair.game.ship.alive)
+
+  if (currentGenerationTimeAlive > maxTimeAlive) {
+    stillAlive.forEach(pair => pair.game.ship.kill())
+  }
+
+  if (stillAlive.length > 0) {
+    const fittest: GameBrainPair = stillAlive.reduce((best, curr) => curr.brain.fitness > best.brain.fitness ? curr : best)
+    gameGraphics.bg()
+
+    stillAlive.forEach(pair => {
+      const brainThoughts: number[] = thinkBrain(pair.brain, pair.game)
+      pair.game.ship.loadInputs(...brainThoughts)
+      pair.game.update()
+    })
+
+    fittest.game.draw()
+
+    gameGraphics.createText(`Generation: ${population.generationCounter}`, 5, gameGraphics.height - 5, '#fff', 10, 'left', 'bottom').draw()
+    gameGraphics.createText(`Alive: ${stillAlive.length} / ${population.popSize}`, 5, gameGraphics.height - 15, '#fff', 10, 'left', 'bottom').draw()
+    gameGraphics.createText(`Asteroids destroyed: ${fittest.game.asteroidCounter}`, 5, gameGraphics.height - 25, '#fff', 10, 'left', 'bottom').draw()
+    gameGraphics.createText(`Alive for: ${Math.round(currentGenerationTimeAlive)} / ${maxTimeAlive} seconds`, 5, gameGraphics.height - 35, '#fff', 10, 'left', 'bottom').draw()
+  } else {
+    currentGenerationTimeAlive = 0
+    population.nextGeneration()
+    if (population.generationCounter > 0) {
+      population.speciate()
+      fittestRecords.push(population.getFittest())
+    }
+    pairings = population.members.map(member => {
+      return {
+        brain: member,
+        game: new AsteroidsGame(gameGraphics)
+      }
+    })
+    addListeners(pairings)
+  }
+  window.requestAnimationFrame(loop)
+}
+
+window.requestAnimationFrame(loop)
