cfec789406915c7d363c54e6b620c970d4ac3e69
Alston <alston1399@gmail.com>
2024-06-07 08:45:52 -0700

    [Implement][Debug] Neat
    Neat's findSolution() now minimizes the fitness function instead of
    maximizing to a scuffed value

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 const graphics = new Graphics().setSize(800, 600).appendTo(document.body)
-const desired = TrainingValues.XOR
-Neat.FindSolution(desired, 0.05).then(solution => {
+const desiredValues = TrainingValues.XOR
+const neat: Neat = new Neat().setGraphics(graphics)
+neat.findSolution(desiredValues, 0.05).then(solution => {
   graphics.bg()
   solution.setGraphics(graphics).draw()
 })
