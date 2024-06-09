e768f85771c570647d4a664bc53a3208ee83f34b
Alston <alston1399@gmail.com>
2024-06-09 11:53:43 -0700

    [Cleanup] Demos
    Removed old /demos/ directory and moved the HTML into /build/ and
    scripts into /src/

--- demos/XORIndexScript.js
+++
@@ -1,7 +0,0 @@
-const graphics = new Graphics().setSize(800, 600).appendTo(document.body);
-const desiredValues = TrainingValues.XOR;
-const neat = new Neat().setGraphics(graphics);
-neat.findSolution(desiredValues, 0.05).then(solution => {
-  graphics.bg();
-  solution.setGraphics(graphics).draw();
-});
