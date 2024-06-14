d26c3245939dbe6afdef38e1bee6d53f1a5f5f28
Alston <alston1399@gmail.com>
2024-06-13 22:38:38 -0700

    [Implement] Raycasting
    Added lookCircles(), lookLines(), and generalized look() to handle any
    object passed into it

--- src/Demos/Raycasting/Particle.ts
+++ src/Demos/Raycasting/Particle.ts
@@ -1,25 +1,18 @@
 class Particle implements Drawable {
-  static NumLines: number = 36
+  static NumLines: number = 360
 
   pos: Vector
   rays: Ray[]
 
   graphics: Graphics
 
-  constructor() {
-    this.pos = new Vector(raycastingGraphics.width / 2, raycastingGraphics.height / 2)
+  constructor(graphics: Graphics) {
+    this.graphics = graphics
+    this.pos = new Vector(graphics.width / 2, graphics.height / 2)
     this.rays = []
     for (let i = 0; i < Particle.NumLines; i++) {
-      this.rays[i] = new Ray(this.pos, lerp(i, 0, Particle.NumLines, 0, 2 * Math.PI))
+      this.rays[i] = new Ray(this.pos, lerp(i, 0, Particle.NumLines, 0, 2 * Math.PI)).setGraphics(graphics)
     }
-  }
-
-  setGraphics(graphics: Graphics): Particle {
-    this.graphics = graphics
-    for (const ray of this.rays) {
-      ray.setGraphics(graphics)
-    }
-    return this
   }
 
   update(x: number, y: number): void {
@@ -27,12 +20,58 @@ class Particle implements Drawable {
     this.pos.y = y
   }
 
-  look(walls: Line[]): void {
-    for (let ray of this.rays) {
+  look(objects: (Line | Circle)[]): void {
+    for (const ray of this.rays) {
       let closest: Vector
       let record: number = Infinity
-      for (let wall of walls) {
+      for (const object of objects) {
+        let point: Vector
+        if (object instanceof Line) {
+          point = ray.castOntoLine(object)
+        }
+        else if (object instanceof Circle) {
+          point = ray.castOntoCircle(object)
+        }
+        if (point) {
+          const distance: number = this.pos.distanceTo(point)
+          if (distance < record) {
+            record = distance
+            closest = point
+          }
+        }
+      }
+      if (closest) {
+        this.graphics.createLine(this.pos.x, this.pos.y, closest.x, closest.y, '#fff').draw()
+      }
+    }
+  }
+
+  lookLines(walls: Line[]): void {
+    for (const ray of this.rays) {
+      let closest: Vector
+      let record: number = Infinity
+      for (const wall of walls) {
         const point: Vector = ray.castOntoLine(wall)
+        if (point) {
+          const d: number = this.pos.distanceTo(point)
+          if (d < record) {
+            record = d
+            closest = point
+          }
+        }
+      }
+      if (closest) {
+        this.graphics.createLine(this.pos.x, this.pos.y, closest.x, closest.y, '#fff').draw()
+      }
+    }
+  }
+
+  lookCircles(circles: Circle[]): void {
+    for (const ray of this.rays) {
+      let closest: Vector
+      let record: number = Infinity
+      for (const circle of circles) {
+        const point: Vector = ray.castOntoCircle(circle)
         if (point) {
           const d: number = this.pos.distanceTo(point)
           if (d < record) {
