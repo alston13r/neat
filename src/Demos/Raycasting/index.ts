6c1448ec2afe6499c0f5c87fc05c2ce183912ff3
Alston <alston1399@gmail.com>
2024-06-13 20:43:50 -0700

    [Cleanup]
    Removed Boundary references, added any necessary setGraphics() calls

--- src/Demos/Raycasting/index.ts
+++ src/Demos/Raycasting/index.ts
@@ -1,16 +1,15 @@
 const raycastingGraphics: Graphics = new Graphics().setSize(800, 600).appendToBody()
 
-let wall = new Boundary(100, 100, 200, 300)
-let particle = new Particle()
+let particle = new Particle().setGraphics(raycastingGraphics)
 
-let walls: Boundary[] = []
+const walls: Line[] = []
 
 for (let i = 0; i < 5; i++) {
   const x1: number = Math.random() * raycastingGraphics.width
   const y1: number = Math.random() * raycastingGraphics.height
   const x2: number = Math.random() * raycastingGraphics.width
   const y2: number = Math.random() * raycastingGraphics.height
-  walls[i] = new Boundary(x1, y1, x2, y2)
+  walls[i] = raycastingGraphics.createLine(x1, y1, x2, y2, '#fff')
 }
 
 let mouseX: number = 0
@@ -23,25 +22,12 @@ window.addEventListener('mousemove', e => {
 
 function raycastLoop() {
   raycastingGraphics.bg()
-
   for (let wall of walls) {
     wall.draw()
   }
-
   particle.update(mouseX, mouseY)
-
   particle.draw()
-
   particle.look(walls)
-
-
-
-
-  // const pt: Vector = ray.cast(wall)
-  // if (pt) {
-  // raycastingGraphics.createCircle(pt.x, pt.y, 4, true, '#fff').draw()
-  // }
-
   window.requestAnimationFrame(raycastLoop)
 }
 
