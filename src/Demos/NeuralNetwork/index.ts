fa3fa9b1eeb93dd06019c111f7bd80026f10450a
Alston <alston1399@gmail.com>
2024-06-17 09:05:53 -0700

    [Demo] NeuralNetwork
    Added a draw loop for the NeuralNetwork demo
    
    Introduced some randomization during the back propagation process
    to hopefully deal with that approach to 0.5 for all inputs that
    sometimes happens

--- src/Demos/NeuralNetwork/index.ts
+++ src/Demos/NeuralNetwork/index.ts
@@ -1,3 +1,5 @@
+const neuralNetGraphics: Graphics = new Graphics().setSize(800, 600).appendToBody()
+
 const desired = TrainingValues.XOR
 
 const neuralNet: NeuralNetwork = new NeuralNetwork(2, 2, 1)
@@ -13,26 +15,57 @@ function getError(): number {
   return error
 }
 
-const iterationCount: number = 200000
-const logCount: number = 100
+const maxIterations: number = 200000
+const loopsPerAnimationFrame: number = 100
 const desiredError: number = 0.05
+let currentIteration: number = 0
 
-for (let i = 0; i < iterationCount; i++) {
-  const error: number = getError()
-  neuralNet.adjustAlpha(error)
+function neuralNetLoop(): void {
+  neuralNetGraphics.bg()
 
-  if (i % (iterationCount / logCount) == 0) {
-    for (let pair of desired.ordered) {
-      console.log('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']')
+  let solutionFound: boolean = false
+
+  for (let i = 0; i < loopsPerAnimationFrame; i++) {
+    if (solutionFound) break
+
+    currentIteration++
+
+    const error: number = getError()
+    neuralNet.adjustAlpha(error)
+
+    if (error <= desiredError) {
+      solutionFound = true
     }
-    console.log('Current error: ' + error)
-  }
-  if (error <= desiredError) {
-    console.log(`Solution found in ${i} iterations`)
-    for (let pair of desired.ordered) {
-      console.log('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']')
+
+    if (error <= desiredError) {
+      solutionFound = true
+      console.log(`Solution found in ${currentIteration} iterations`)
+      for (let pair of desired.ordered) {
+        console.log('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']')
+      }
+      break
     }
-    break
+    neuralNet.backPropagation(desired)
   }
-  neuralNet.backPropagation(desired)
+
+  if (Math.random() < 0.005) {
+    console.log('mutate')
+    neuralNet.mutateWeights()
+    neuralNet.mutateBiases()
+    neuralNet.mutateActivationFunctions()
+  }
+
+  neuralNetGraphics.createText(`Current iteration: ${currentIteration}`, 5, 5, '#fff', 20, 'left', 'top').draw()
+
+  for (let [i, pair] of desired.ordered.entries()) {
+    neuralNetGraphics.createText(
+      '[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']',
+      5, 25 + i * 20, '#fff', 20, 'left', 'top'
+    ).draw()
+  }
+
+  if (solutionFound) neuralNetGraphics.createText('solution', 5, 105, '#fff', 20, 'left', 'top').draw()
+  else if (currentIteration < maxIterations) window.requestAnimationFrame(neuralNetLoop)
 }
+
+window.requestAnimationFrame(neuralNetLoop)
