c8c05b54476fba805d9497f9277009f9bc220872
Alston <alston1399@gmail.com>
2024-06-17 09:07:00 -0700

    [Build]

--- build/Demos/NeuralNetwork/index.js
+++ build/Demos/NeuralNetwork/index.js
@@ -1,3 +1,4 @@
+const neuralNetGraphics = new Graphics().setSize(800, 600).appendToBody();
 const desired = TrainingValues.XOR;
 const neuralNet = new NeuralNetwork(2, 2, 1);
 function getError() {
@@ -10,25 +11,46 @@ function getError() {
     }
     return error;
 }
-const iterationCount = 200000;
-const logCount = 100;
+const maxIterations = 200000;
+const loopsPerAnimationFrame = 100;
 const desiredError = 0.05;
-for (let i = 0; i < iterationCount; i++) {
-    const error = getError();
-    neuralNet.adjustAlpha(error);
-    if (i % (iterationCount / logCount) == 0) {
-        for (let pair of desired.ordered) {
-            console.log('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']');
+let currentIteration = 0;
+function neuralNetLoop() {
+    neuralNetGraphics.bg();
+    let solutionFound = false;
+    for (let i = 0; i < loopsPerAnimationFrame; i++) {
+        if (solutionFound)
+            break;
+        currentIteration++;
+        const error = getError();
+        neuralNet.adjustAlpha(error);
+        if (error <= desiredError) {
+            solutionFound = true;
         }
-        console.log('Current error: ' + error);
-    }
-    if (error <= desiredError) {
-        console.log(`Solution found in ${i} iterations`);
-        for (let pair of desired.ordered) {
-            console.log('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']');
+        if (error <= desiredError) {
+            solutionFound = true;
+            console.log(`Solution found in ${currentIteration} iterations`);
+            for (let pair of desired.ordered) {
+                console.log('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']');
+            }
+            break;
         }
-        break;
+        neuralNet.backPropagation(desired);
     }
-    neuralNet.backPropagation(desired);
+    if (Math.random() < 0.005) {
+        console.log('mutate');
+        neuralNet.mutateWeights();
+        neuralNet.mutateBiases();
+        neuralNet.mutateActivationFunctions();
+    }
+    neuralNetGraphics.createText(`Current iteration: ${currentIteration}`, 5, 5, '#fff', 20, 'left', 'top').draw();
+    for (let [i, pair] of desired.ordered.entries()) {
+        neuralNetGraphics.createText('[' + pair.inputs.join(', ') + '] -> [' + neuralNet.feedForward(pair.inputs).join(', ') + ']', 5, 25 + i * 20, '#fff', 20, 'left', 'top').draw();
+    }
+    if (solutionFound)
+        neuralNetGraphics.createText('solution', 5, 105, '#fff', 20, 'left', 'top').draw();
+    else if (currentIteration < maxIterations)
+        window.requestAnimationFrame(neuralNetLoop);
 }
+window.requestAnimationFrame(neuralNetLoop);
 //# sourceMappingURL=index.js.map
