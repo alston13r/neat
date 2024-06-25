35be0065a76138c21569f3468a7e6e283b727157
Alston <alston1399@gmail.com>
2024-06-24 17:32:38 -0700

    [Skeleton] Delta time

--- src/Demos/DeltaTime/index.ts
+++ src/Demos/DeltaTime/index.ts
@@ -2,4 +2,13 @@ const asteroidsGraphics: Graphics = new Graphics().setSize(800, 600).appendToBod
 const asteroidsGame: Asteroids = new Asteroids()
 
 asteroidsGame.setGraphics(asteroidsGraphics)
-asteroidsGame.draw()
+
+let lastDeltaTimeTimestamp: number = 0
+
+function deltaTimeLoop(timestamp?: number): void {
+  const deltaTime: number = timestamp - lastDeltaTimeTimestamp
+  lastDeltaTimeTimestamp = timestamp
+
+  asteroidsGame.draw()
+  asteroidsGraphics.createText(deltaTime.toString(), 5, 5, '#fff', 20, 'left', 'top')
+}
