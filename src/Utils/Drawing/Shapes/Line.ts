649294e3fe94d8affb33a1ad07edb23898ee8d48
Alston <alston1399@gmail.com>
2024-07-22 01:47:08 -0700

    [Refactor] Line

--- src/Utils/Drawing/Shapes/Line.ts
+++ src/Utils/Drawing/Shapes/Line.ts
@@ -1,39 +1,42 @@
-class Line implements HasTwoPoints, Drawable {
+class Line implements HasTwoPoints, Drawable, HasPath {
   pos1: Vec2
   pos2: Vec2
-  graphics: Graphics
-  color: string
-  lineWidth: number
 
-  constructor(graphics: Graphics, x1: number, y1: number, x2: number, y2: number,
-    options: LineGraphicsOptions = {}) {
-    this.graphics = graphics
+  constructor(x1: number, y1: number, x2: number, y2: number) {
     this.pos1 = vec2.fromValues(x1, y1)
     this.pos2 = vec2.fromValues(x2, y2)
-    this.color = options.color || '#0f0'
-    this.lineWidth = options.lineWidth || 1
   }
 
-  get x1(): number {
+  get x1() {
     return this.pos1[0]
   }
 
-  get y1(): number {
+  get y1() {
     return this.pos1[1]
   }
 
-  get x2(): number {
+  get x2() {
     return this.pos2[0]
   }
 
-  get y2(): number {
+  get y2() {
     return this.pos2[1]
   }
 
-  draw(): void {
-    const ctx: CanvasRenderingContext2D = this.graphics.ctx
-    ctx.strokeStyle = this.color
-    ctx.lineWidth = this.lineWidth
-    graphics.line(ctx, this.pos1, this.pos2)
+  stroke(g: Graphics): void {
+    g.line(this.x1, this.y1, this.x2, this.y2)
+  }
+
+  createPath(): Path2D {
+    let path = new Path2D()
+    path.moveTo(this.x1, this.y1)
+    path.lineTo(this.x2, this.y2)
+    return path
+  }
+
+  appendToPath(path: Path2D): Path2D {
+    path.moveTo(this.x1, this.y1)
+    path.lineTo(this.x2, this.y2)
+    return path
   }
 }
