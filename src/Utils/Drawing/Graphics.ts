cfeb9e902b6cba273f21358643906efcab464121
Alston <alston1399@gmail.com>
2024-07-22 01:52:47 -0700

    [Refactor] Graphics
    Removed "static" graphics-like methods and just put them into the
    Graphics class as instance methods
    
    The parameters are explicit numbers and the shapes classes have their
    own optional methods for drawing, filling, and/or stroking with the
    specified graphics object passed in as a parameter

--- src/Utils/Drawing/Graphics.ts
+++ src/Utils/Drawing/Graphics.ts
@@ -1,176 +1,196 @@
-const graphics = {
-  createCanvas(): HTMLCanvasElement {
-    return document.createElement('canvas')
-  },
-
-  setSize(canvas: HTMLCanvasElement, size: ReadonlyVec2): HTMLCanvasElement {
-    canvas.width = size[0]
-    canvas.height = size[1]
-    return canvas
-  },
-
-  getSize(canvas: HTMLCanvasElement): ReadonlyVec2 {
-    return vec2.fromValues(canvas.width, canvas.height)
-  },
-
-  line(context: CanvasRenderingContext2D, pos1: ReadonlyVec2, pos2: ReadonlyVec2) {
-    context.beginPath()
-    context.moveTo(pos1[0], pos1[1])
-    context.lineTo(pos2[0], pos2[1])
-    context.stroke()
-  },
-
-  strokeCircle(context: CanvasRenderingContext2D, pos: ReadonlyVec2, radius: number) {
-    context.beginPath()
-    context.ellipse(pos[0], pos[1], radius, radius, 0, 0, TwoPi)
-    context.stroke()
-  },
-
-  fillCircle(context: CanvasRenderingContext2D, pos: ReadonlyVec2, radius: number) {
-    context.beginPath()
-    context.ellipse(pos[0], pos[1], radius, radius, 0, 0, TwoPi)
-    context.fill()
-  },
-
-  strokeRect(context: CanvasRenderingContext2D, pos: ReadonlyVec2, size: ReadonlyVec2) {
-    context.strokeRect(pos[0], pos[1], size[0], size[1])
-  },
-
-  fillRect(context: CanvasRenderingContext2D, pos: ReadonlyVec2, size: ReadonlyVec2) {
-    context.fillRect(pos[0], pos[1], size[0], size[1])
-  },
-
-  strokeTriangle(context: CanvasRenderingContext2D, pos1: ReadonlyVec2, pos2: ReadonlyVec2, pos3: ReadonlyVec2) {
-    context.beginPath()
-    context.moveTo(pos1[0], pos1[1])
-    context.lineTo(pos2[0], pos2[1])
-    context.lineTo(pos3[0], pos3[1])
-    context.closePath()
-    context.stroke()
-  },
-
-  fillTriangle(context: CanvasRenderingContext2D, pos1: ReadonlyVec2, pos2: ReadonlyVec2, pos3: ReadonlyVec2) {
-    context.beginPath()
-    context.moveTo(pos1[0], pos1[1])
-    context.lineTo(pos2[0], pos2[1])
-    context.lineTo(pos3[0], pos3[1])
-    context.closePath()
-    context.fill()
-  },
-
-  strokePolygon(context: CanvasRenderingContext2D, points: ReadonlyVec2[], nPoints: number) {
-    if (nPoints <= 1) return
-    if (nPoints == 2) {
-      graphics.line(context, points[0], points[1])
-    } else {
-      context.beginPath()
-      for (let i = 0; i < nPoints; i++) {
-        const point = points[i]
-        if (i == 0) context.moveTo(point[0], point[1])
-        else context.lineTo(point[0], point[1])
-      }
-      context.closePath()
-      context.stroke()
-    }
-  },
-
-  fillPolygon(context: CanvasRenderingContext2D, points: ReadonlyVec2[], nPoints: number) {
-    if (nPoints <= 1) return
-    if (nPoints == 2) {
-      graphics.line(context, points[0], points[1])
-    } else {
-      context.beginPath()
-      for (let i = 0; i < nPoints; i++) {
-        const point = points[i]
-        if (i == 0) context.moveTo(point[0], point[1])
-        else context.lineTo(point[0], point[1])
-      }
-      context.closePath()
-      context.fill()
-    }
-  },
-
-  strokeText(context: CanvasRenderingContext2D, text: string, pos: ReadonlyVec2) {
-    context.strokeText(text, pos[0], pos[1])
-  },
-
-  fillText(context: CanvasRenderingContext2D, text: string, pos: ReadonlyVec2) {
-    context.fillText(text, pos[0], pos[1])
-  },
-
-  clearRect(context: CanvasRenderingContext2D, pos: ReadonlyVec2, size: ReadonlyVec2): void {
-    context.clearRect(pos[0], pos[1], size[0], size[1])
-  }
-}
-
 class Graphics {
   canvas: HTMLCanvasElement
-  ctx: CanvasRenderingContext2D
+  context: CanvasRenderingContext2D
+  drawQueues = new Set<DrawQueue>()
 
   constructor(canvas?: HTMLCanvasElement) {
-    this.canvas = canvas || graphics.createCanvas()
-    this.ctx = this.canvas.getContext('2d')
+    this.canvas = canvas || document.createElement('canvas')
+    this.context = this.canvas.getContext('2d')
   }
 
-  setSize(width: number, height: number): Graphics {
-    graphics.setSize(this.canvas, vec2.fromValues(width, height))
+  setSize(width: number, height: number) {
+    this.canvas.width = width
+    this.canvas.height = height
     return this
   }
 
-  appendTo(ele: HTMLElement): Graphics {
+  appendTo(ele: HTMLElement) {
     ele.appendChild(this.canvas)
     return this
   }
 
-  appendToBody(): Graphics {
+  appendToBody() {
     document.body.appendChild(this.canvas)
     return this
   }
 
-  get width(): number {
+  get width() {
     return this.canvas.width
   }
 
-  get height(): number {
+  get height() {
     return this.canvas.height
   }
 
-  get size(): Vec2 {
-    return graphics.getSize(this.canvas)
+  get size() {
+    return vec2.fromValues(this.width, this.height)
   }
 
-  createCircle(x: number, y: number, radius: number = 10, options?: CircleGraphicsOptions): Circle {
-    return new Circle(this, x, y, radius, options)
+  get fillStyle() {
+    return this.context.fillStyle
   }
 
-  createLine(x1: number, y1: number, x2: number, y2: number, options?: LineGraphicsOptions): Line {
-    return new Line(this, x1, y1, x2, y2, options)
+  get strokeStyle() {
+    return this.context.strokeStyle
   }
 
-  createRectangle(x: number, y: number, width: number, height: number,
-    options?: RectangleGraphicsOptions): Rectangle {
-    return new Rectangle(this, x, y, width, height, options)
+  set fillStyle(fillStyle) {
+    this.context.fillStyle = fillStyle
   }
 
-  createTriangle(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number,
-    options?: TriangleGraphicsOptions): TriangleGraphics {
-    return new TriangleGraphics(this, x1, y1, x2, y2, x3, y3, options)
+  set strokeStyle(strokeStyle) {
+    this.context.strokeStyle = strokeStyle
   }
 
-  createPolygon(points: Vec2[], options?: PolygonGraphicsOptions): Polygon {
-    return new Polygon(this, points, options)
+  get lineWidth() {
+    return this.context.lineWidth
   }
 
-  createText(text: string, x: number, y: number, options?: TextGraphicsOptions): TextGraphics {
-    return new TextGraphics(this, text, x, y, options)
+  set lineWidth(lineWidth) {
+    this.context.lineWidth = lineWidth
   }
 
-  bg(color = '#000'): void {
-    this.ctx.fillStyle = color
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+  get textAlign() {
+    return this.context.textAlign
   }
 
-  clear(): void {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+  get textBaseline() {
+    return this.context.textBaseline
+  }
+
+  set textAlign(textAlign) {
+    this.context.textAlign = textAlign
+  }
+
+  set textBaseline(textBaseline) {
+    this.context.textBaseline = textBaseline
+  }
+
+  get font() {
+    return this.context.font
+  }
+
+  set font(font) {
+    this.context.font = font
+  }
+
+  fill(fillRule?: CanvasFillRule): void
+  fill(path: Path2D, fillRule?: CanvasFillRule): void
+  fill(a: CanvasFillRule | Path2D, b?: CanvasFillRule) {
+    if (a instanceof Path2D) this.context.fill(a, b)
+    else this.context.fill(a)
+  }
+
+  stroke(path?: Path2D) {
+    this.context.stroke(path)
+  }
+
+  fillCircle(x: number, y: number, radius: number) {
+    this.context.beginPath()
+    this.context.arc(x, y, radius, 0, TwoPi)
+    this.context.fill()
+  }
+
+  strokeCircle(x: number, y: number, radius: number) {
+    this.context.beginPath()
+    this.context.arc(x, y, radius, 0, TwoPi)
+    this.context.stroke()
+  }
+
+  line(x1: number, y1: number, x2: number, y2: number) {
+    this.context.beginPath()
+    this.context.moveTo(x1, y1)
+    this.context.lineTo(x2, y2)
+    this.context.stroke()
+  }
+
+  fillRect(x: number, y: number, width: number, height: number) {
+    this.context.fillRect(x, y, width, height)
+  }
+
+  strokeRect(x: number, y: number, width: number, height: number) {
+    this.context.strokeRect(x, y, width, height)
+  }
+
+  fillTriangle(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number) {
+    this.context.beginPath()
+    this.context.moveTo(x1, y1)
+    this.context.lineTo(x2, y2)
+    this.context.lineTo(x3, y3)
+    this.context.closePath()
+    this.context.fill()
+  }
+
+  strokeTriangle(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number) {
+    this.context.beginPath()
+    this.context.moveTo(x1, y1)
+    this.context.lineTo(x2, y2)
+    this.context.lineTo(x3, y3)
+    this.context.closePath()
+    this.context.stroke()
+  }
+
+  fillPolygon(points: ReadonlyVec2[]) {
+    const nPoints = points.length
+    if (nPoints <= 2) return
+    const p1 = points[0]
+    this.context.moveTo(p1[0], p1[1])
+    for (let i = 1; i < length; i++) {
+      const p = points[i]
+      this.context.lineTo(p[0], p[1])
+    }
+    this.context.closePath()
+    this.context.fill()
+  }
+
+  strokePolygon(points: ReadonlyVec2[]) {
+    const nPoints = points.length
+    if (nPoints <= 2) return
+    const p1 = points[0]
+    this.context.moveTo(p1[0], p1[1])
+    for (let i = 1; i < length; i++) {
+      const p = points[i]
+      this.context.lineTo(p[0], p[1])
+    }
+    this.context.closePath()
+    this.context.stroke()
+  }
+
+  fillText(text: string, x: number, y: number) {
+    this.context.fillText(text, x, y)
+  }
+
+  strokeText(text: string, x: number, y: number) {
+    this.context.strokeText(text, x, y)
+  }
+
+  bg(color = '#000') {
+    this.context.fillStyle = color
+    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+  }
+
+  clearRect(x: number, y: number, width: number, height: number) {
+    this.context.clearRect(x, y, width, height)
+  }
+
+  clear() {
+    this.context.clearRect(0, 0, this.width, this.height)
+  }
+
+  initDrawQueue(color: string | CanvasGradient | CanvasPattern = '#fff',
+    filling = false, stroking = true, lineWidth = 1) {
+    let queue = new DrawQueue(this, color, filling, stroking, lineWidth)
+    this.drawQueues.add(queue)
+    return queue
   }
 }
