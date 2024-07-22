a60dc8560e10ed143c9bc52b545fba8121b60a5d
Alston <alston1399@gmail.com>
2024-07-22 01:46:21 -0700

    [Refactor] Circle

--- src/Utils/Drawing/Shapes/Circle.ts
+++ src/Utils/Drawing/Shapes/Circle.ts
@@ -1,42 +1,37 @@
-class Circle implements HasPoint, Drawable {
+class Circle implements Drawable, HasPath {
   pos: Vec2
-  graphics: Graphics
   radius: number
-  fill: boolean
-  color: string
-  stroke: boolean
-  lineWidth: number
 
-  constructor(graphics: Graphics, x: number, y: number, radius: number = 10,
-    options: CircleGraphicsOptions = {}) {
-    this.graphics = graphics
+  constructor(x: number, y: number, radius: number) {
     this.pos = vec2.fromValues(x, y)
     this.radius = radius
-    this.fill = options.fill == undefined ? true : options.fill
-    this.color = options.color || '#fff'
-    this.stroke = options.stroke == undefined ? false : options.stroke
-    this.lineWidth = options.lineWidth || 1
   }
 
-  get x(): number {
+  get x() {
     return this.pos[0]
   }
 
-  get y(): number {
+  get y() {
     return this.pos[1]
   }
 
-  draw(): void {
-    if (!this.fill && !this.stroke) return
-    const ctx: CanvasRenderingContext2D = this.graphics.ctx
-    if (this.fill) {
-      ctx.fillStyle = this.color
-      graphics.fillCircle(ctx, this.pos, this.radius)
-    }
-    if (this.stroke) {
-      ctx.lineWidth = this.lineWidth
-      ctx.strokeStyle = this.color
-      graphics.strokeCircle(ctx, this.pos, this.radius)
-    }
+  fill(g: Graphics) {
+    g.fillCircle(this.x, this.y, this.radius)
+  }
+
+  stroke(g: Graphics) {
+    g.strokeCircle(this.x, this.y, this.radius)
+  }
+
+  createPath(): Path2D {
+    let path = new Path2D()
+    path.arc(this.x, this.y, this.radius, 0, TwoPi)
+    return path
+  }
+
+  appendToPath(path: Path2D): Path2D {
+    path.moveTo(this.x, this.y)
+    path.arc(this.x, this.y, this.radius, 0, TwoPi)
+    return path
   }
 }
