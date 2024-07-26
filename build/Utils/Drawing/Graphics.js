a6756a7c6c184644b22eeb062b69c068d009ec70
Alston <alston1399@gmail.com>
2024-07-25 19:42:37 -0700

    [Build]

--- build/Utils/Drawing/Graphics.js
+++ build/Utils/Drawing/Graphics.js
@@ -180,26 +180,4 @@ class Graphics {
         setTimeout(() => rec.stop(), time);
     }
 }
-// startRecording(canvas: HTMLCanvasElement, time: number) {
-//   const chunks: Blob[] = [] // here we will store our recorded media chunks (Blobs)
-//   const stream = canvas.captureStream() // grab our canvas MediaStream
-//   const rec = new MediaRecorder(stream) // init the recorder
-//   // every time the recorder has new data, we will store it in our array
-//   rec.ondataavailable = e => chunks.push(e.data)
-//   // only when the recorder stops, we construct a complete Blob from all the chunks
-//   rec.onstop = () => this.exportVid(new Blob(chunks, { type: 'video/webm' }))
-//   rec.start()
-//   setTimeout(() => rec.stop(), time) // stop recording in 3s
-// },
-// exportVid(blob: Blob) {
-//   const vid = document.createElement('video');
-//   vid.src = URL.createObjectURL(blob);
-//   vid.controls = true;
-//   document.body.appendChild(vid);
-//   const a = document.createElement('a');
-//   a.download = 'myvid.webm';
-//   a.href = vid.src;
-//   a.textContent = 'download the video';
-//   document.body.appendChild(a);
-// }
 //# sourceMappingURL=Graphics.js.map
