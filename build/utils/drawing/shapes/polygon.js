8b3a1175e590608887824be732c51208d4db5b70
Alston <alston1399@gmail.com>
2024-07-29 00:05:06 -0700

    [Rename]

---
+++ build/utils/drawing/shapes/polygon.js
@@ -0,0 +1,31 @@
+class Polygon {
+    points;
+    constructor(points) {
+        this.points = points;
+    }
+    fill(g) {
+        g.fillPolygon(this.points);
+    }
+    stroke(g) {
+        g.strokePolygon(this.points);
+    }
+    createPath() {
+        let length = this.points.length;
+        if (this.points.length == 0 || this.points.length == 1)
+            return new Path2D();
+        const path = new Path2D();
+        const p1 = this.points[0];
+        path.moveTo(p1[0], p1[1]);
+        for (let i = 1; i < length; i++) {
+            const p = this.points[i];
+            path.lineTo(p[0], p[1]);
+        }
+        path.closePath();
+        return path;
+    }
+    appendToPath(path) {
+        path.addPath(this.createPath());
+        return path;
+    }
+}
+//# sourceMappingURL=polygon.js.map
---
+++ build/utils/drawing/shapes/triangle.js
@@ -0,0 +1,47 @@
+class Triangle {
+    pos1;
+    pos2;
+    pos3;
+    constructor(x1, y1, x2, y2, x3, y3) {
+        this.pos1 = vec2.fromValues(x1, y1);
+        this.pos2 = vec2.fromValues(x2, y2);
+        this.pos3 = vec2.fromValues(x3, y3);
+    }
+    get x1() {
+        return this.pos1[0];
+    }
+    get y1() {
+        return this.pos1[1];
+    }
+    get x2() {
+        return this.pos2[0];
+    }
+    get y2() {
+        return this.pos2[1];
+    }
+    get x3() {
+        return this.pos3[0];
+    }
+    get y3() {
+        return this.pos3[1];
+    }
+    fill(g) {
+        g.fillTriangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
+    }
+    stroke(g) {
+        g.strokeTriangle(this.x1, this.y1, this.x2, this.y2, this.x3, this.y3);
+    }
+    createPath() {
+        let path = new Path2D();
+        path.moveTo(this.x1, this.y1);
+        path.lineTo(this.x2, this.y2);
+        path.lineTo(this.x3, this.y3);
+        path.closePath();
+        return path;
+    }
+    appendToPath(path) {
+        path.addPath(this.createPath());
+        return path;
+    }
+}
+//# sourceMappingURL=triangle.js.map
