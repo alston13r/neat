86c2bf5ff42fb98ffeb20d66be3c49d614339ad6
Alston <alston1399@gmail.com>
2024-07-29 15:02:02 -0700

    [Optimize] Benchmark
    Added code for benchmarking Species.Compare

---
+++ src/benchmark/index.ts
@@ -0,0 +1,39 @@
+function stressTest(name: string, fn: Function) {
+  const start = performance.now()
+  fn()
+  const end = performance.now()
+  const diff = end - start
+  console.log(name + ': ' + diff + ' ms')
+  return diff
+}
+
+function calcFitness(brain: Brain) {
+  brain.fitness = 0
+  for (const value of TrainingValues.XOR.random) {
+    const actual = brain.think(value.inputs)
+    const errors = value.outputs.map((expected, i) => lerp(Math.abs(expected - actual[i]), 0, 2, 1, 0))
+    errors.forEach(error => brain.fitness += error)
+  }
+}
+
+const results: number[] = new Array(1000)
+
+const stressPop = new Population(1000, 2, 0, 1)
+for (let i = 0; i < 1000; i++) {
+  console.log(i)
+  stressPop.nextGeneration()
+  stressPop.members.forEach(calcFitness)
+  stressPop.updateFittestEver()
+  const start = performance.now()
+  stressPop.speciate()
+  const end = performance.now()
+  results[i] = end - start
+}
+
+const resultsSum = results.reduce((sum, curr) => sum + curr)
+const resultsAvg = resultsSum / 1000
+const resultsDeviation = Math.sqrt(results.reduce((sum, curr) => sum + (curr - resultsAvg) ** 2) / 1000)
+
+console.log(`Total time: ${resultsSum}`)
+console.log(`Average time: ${resultsAvg}`)
+console.log(`Standard deviation: ${resultsDeviation}`)
