f04d6547a6df5d6d7deb1a693bf97e45c12b7416
Alston <alston1399@gmail.com>
2024-07-29 00:28:15 -0700

    [Debug] Brain
    Reduced the chance to add a new node to 1%

--- src/brain/brain.ts
+++ src/brain/brain.ts
@@ -21,7 +21,7 @@ class Brain {
   /** Toggle for new nodes */
   static AllowNewNodes = true
   /** The chance for a new node to be made */
-  static AddANodeChance = 0.03
+  static AddANodeChance = 0.01
 
   /** The current fitness of the brain */
   fitness = 0
