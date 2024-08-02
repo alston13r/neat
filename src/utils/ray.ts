4c28c75cde106867587666ef96cd800dd5f2e6f0
Alston <alston1399@gmail.com>
2024-08-02 03:52:31 -0700

    [Optimize] Ray

--- src/utils/ray.ts
+++ src/utils/ray.ts
@@ -1,23 +1,21 @@
 type CastableObject = Line | Circle
 
-class Ray2 implements Drawable {
+class Ray2 {
   pos: Vec2
   dir: Vec2
+  posPlusDir: Vec2 = vec2.create()
   length: number
 
   constructor(pos: Vec2, angle = 0, length = 1) {
     this.pos = pos
-    this.dir = vec2.fromValues(Math.cos(angle), Math.sin(angle))
+    this.dir = FastVec2FromRadian(angle)
+    vec2.add(this.posPlusDir, pos, this.dir)
     this.length = length
   }
 
-  lookAt(x: number, y: number): void {
-    vec2.subtract(this.dir, vec2.fromValues(x, y), this.pos)
-    vec2.normalize(this.dir, this.dir)
-  }
-
   setAngle(angle: number): Ray2 {
-    vec2.set(this.dir, Math.cos(angle), Math.sin(angle))
+    vec2.copy(this.dir, FastVec2FromRadian(angle))
+    vec2.add(this.posPlusDir, this.pos, this.dir)
     return this
   }
 
@@ -38,7 +36,7 @@ class Ray2 implements Drawable {
         point = this.castOntoCircle(object)
       }
       if (point) {
-        const distance = vec2.distance(this.pos, point)
+        const distance = vec2.squaredDistance(this.pos, point)
         if (distance < record) {
           record = distance
           closest = point
@@ -73,43 +71,55 @@ class Ray2 implements Drawable {
     }
   }
 
+  castOntoCircles(circles: Circle[]) {
+    let closest: Vec2
+    let record = Infinity
+    for (const circle of circles) {
+      let point = this.castOntoCircle(circle)
+      if (point) {
+        const distance = vec2.squaredDistance(this.pos, point)
+        if (distance < record) {
+          record = distance
+          closest = point
+        }
+      }
+    }
+    return closest
+  }
+
   castOntoCircle(circle: Circle): Vec2 {
     const px = this.pos[0]
     const py = this.pos[1]
     const dx = this.dir[0]
     const dy = this.dir[1]
+    const cx = circle.pos[0]
+    const cy = circle.pos[1]
 
-    const x1 = px - circle.x
-    const y1 = py - circle.y
-    const x2 = px + dx - circle.x
-    const y2 = py + dy - circle.y
-
-    const dr = Math.sqrt(dx ** 2 + dy ** 2)
+    const x1 = px - cx
+    const y1 = py - cy
+    const x2 = x1 + dx
+    const y2 = y1 + dy
 
     const det = x1 * y2 - x2 * y1
-    const disc = circle.radius ** 2 * dr ** 2 - det ** 2
+    const disc = circle.radius - det ** 2
 
     if (disc < 0) return
     const discSqrt = Math.sqrt(disc)
 
     const sgn = dy < 0 ? -1 : 1
 
-    const P = (det * dy + sgn * dx * discSqrt) / dr ** 2
-    const Q = (-det * dx + Math.abs(dy) * discSqrt) / dr ** 2
-    const R = (det * dy - sgn * dx * discSqrt) / dr ** 2
-    const S = (-det * dx - Math.abs(dy) * discSqrt) / dr ** 2
+    const P = (det * dy + sgn * dx * discSqrt) + cx
+    const Q = (-det * dx + Math.abs(dy) * discSqrt) + cy
+    const R = (det * dy - sgn * dx * discSqrt) + cx
+    const S = (-det * dx - Math.abs(dy) * discSqrt) + cy
 
     const p1 = vec2.fromValues(P, Q)
     const p2 = vec2.fromValues(R, S)
-    vec2.add(p1, p1, circle.pos)
-    vec2.add(p2, p2, circle.pos)
 
-    const posAddDir = vec2.add(vec2.create(), this.pos, this.dir)
-
-    const d1 = vec2.distance(this.pos, p1)
-    const d2 = vec2.distance(posAddDir, p1)
-    const d3 = vec2.distance(this.pos, p2)
-    const d4 = vec2.distance(posAddDir, p2)
+    const d1 = vec2.squaredDistance(this.pos, p1)
+    const d2 = vec2.squaredDistance(this.posPlusDir, p1)
+    const d3 = vec2.squaredDistance(this.pos, p2)
+    const d4 = vec2.squaredDistance(this.posPlusDir, p2)
 
     const p1Forward = d2 < d1
     const p2Forward = d4 < d3
@@ -120,11 +130,8 @@ class Ray2 implements Drawable {
     return d1 < d3 ? p1 : p2
   }
 
-  draw(g: Graphics): void {
-    const d = vec2.copy(vec2.create(), this.dir)
-    vec2.scale(d, d, this.length)
-    vec2.add(d, d, this.pos)
-
+  draw(g: Graphics) {
+    const d = vec2.scaleAndAdd([], this.pos, this.dir, this.length)
     g.line(this.pos[0], this.pos[1], d[0], d[1])
   }
 }
