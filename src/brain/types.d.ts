2b0820f3b021a8197261e053dbab65381a1f404d
Alston <alston1399@gmail.com>
2024-08-11 05:54:27 -0700

    [Implement] Brain
    Added serialization
    TODO: test serialization

--- src/brain/types.d.ts
+++ src/brain/types.d.ts
@@ -1,3 +1,10 @@
+declare interface BrainSerial {
+  /** [id, bias, activation function, layer] */
+  nodes: number[]
+  /** [id in, id out, enabled, weight, innovation] */
+  connections: number[]
+}
+
 declare interface RouletteWheelItem {
   brain: BrainOOP
   value: number
