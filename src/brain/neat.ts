2b1444a573923f227f91b15b427982b2601ca173
Alston Rodriguez <alston1399@gmail.com>
2025-04-18 02:39:16 -0400

    [Implement][Docs] neat
    Swapped the parameters passed to the constructor
    
    Copied / modified documentation from the original classes for configs

--- src/brain/neat.ts
+++ src/brain/neat.ts
@@ -1,47 +1,81 @@
-type BrainTopology = {
+/**
+ * Configuration type relating to Brain topology within the Neat algorithm.
+ */
+type BrainTopologyConfig = {
+  /** The number of input nodes that members of the population will be initialized with */
   inputSize: number,
+  /** The number of hidden nodes that members of the population will be initialized with */
   hiddenSize?: number,
+  /** The number of output nodes that members of the population will be initialized with */
   outputSize: number
+  /** The chance for the initial connections made in a member (in the first generation) to be enabled */
   enableChance?: number
 }
 
+/**
+ * Configuration type relating to Mutations within the Neat algorithm.
+ */
 type MutationConfig = {
+  /** Toggle for allowing new connections to be made */
   allowNewConnections?: boolean,
+  /** Toggle for connections to be disabled */
   allowDisablingConnections?: boolean,
+  /** Toggle for allowing recurrent connections */
   allowRecurrentConnections?: boolean,
-
+  /** The chance for a new connection to be made */
   addConnectionChance?: number,
+  /** The chance for an existing connection to be disabled */
   disableConnectionChance?: number,
+  /** The chance for a disabled connection to be reenabled */
   reenableConnectionChance?: number
-
+  /** Toggle for allowing new nodes to be made */
   allowNewNodes?: boolean,
+  /** The chance for a new node to be made */
   addNodeChance?: number
-
+  /** Toggle for allowing connection weight mutations */
   allowWeightMutations?: boolean
+  /** The chance for a connection's weight to be mutated */
   mutateWeightChance?: number,
+  /** The chance for a connection's weight mutation to just nudge the value, as opposed to complete randomization */
   nudgeWeightChange?: number
 }
 
+/**
+ * Configuration type relating to Connections within the Neat algorithm.
+ */
 type ConnectionConfig = {
+  /** The minimum value that a Connection's weight can be */
   minimumWeightValue?: number,
+  /** The maximum value that a Connection's weight can be */
   maximumWeightValue?: number
 }
 
+/**
+ * Configuration type relating to Species within the Neat algorithm.
+ */
 type SpeciesConfig = {
+  /** Toggle for whether or not speciation should occur between generations */
   enabled?: boolean,
+  /** Toggle for if elite members of a species should be preserved between generations */
   elitism?: boolean,
+  /** The percentage of elite members that are preserved between generations */
   elitePercentage?: number
-
+  /** The amount of influence that excess connections have when computing the compatibility between two Brain topologies */
   excessFactor?: number,
+  /** The amount of influence that disjoint connections have when computing the compatibility between two Brain topologies */
   disjointFactor?: number,
+  /** The amount of influence that the average weight difference has when computing the compatibility between two Brain topologies  */
   weightFactor?: number,
+  /** The number of generations that a species can run for, simultaneously, without improvement before being penalized */
   generationPenalization?: number,
+  /** The target number of species to be created, this target will not always be reached but serves as a direction for the dynamic threshold to adjust */
   targetSpecies?: number
+  /** The step size of the dynamic threshold to take when the target number of species is not met */
   dynamicThresholdStepSize?: number
 }
 
 class Neat {
-  topology: BrainTopology
+  topology: BrainTopologyConfig
 
   mutationConfig: MutationConfig = {
     allowNewConnections: true,
@@ -80,7 +114,7 @@ class Neat {
 
   size: number
 
-  constructor(topology: BrainTopology, size: number) {
+  constructor(size: number, topology: BrainTopologyConfig) {
     this.topology = {
       inputSize: topology.inputSize,
       hiddenSize: topology.hiddenSize ?? 0,
@@ -112,3 +146,5 @@ class Neat {
     return this
   }
 }
+
+new Neat(100, { inputSize: 2, outputSize: 1 })
