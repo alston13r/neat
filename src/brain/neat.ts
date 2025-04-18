7800d0a280c0478fa81f1a39523f178a79bd1c78
Alston Rodriguez <alston1399@gmail.com>
2025-04-18 01:54:55 -0400

    [Implement] neat
    Outlining improved neat api

--- src/brain/neat.ts
+++ src/brain/neat.ts
@@ -1,2 +1,46 @@
 // initialize
+//   population size
+//   brain input size
+//   brain hidden size
+//   brain output size
+
 //   settings?
+
+//     brain
+//       initialization
+//         input size
+//         hidden size
+//         output size
+//         enabled chance
+//       mutations
+//         allow new connections
+//         allow disabling connections
+//         allow recurrent connections
+//         add connection chance
+//         disable connection chance
+//         reenable connection chance
+//         allow new nodes
+//         add node chance
+
+//     connection
+//       allow weight mutations
+//       minimum weight value
+//       maximum weight value
+//       mutate weight chance
+//       nudge weight chance
+
+//     population
+//       speciation
+//       elitism
+//       elite percent
+
+//       population size
+
+//     species
+//       excess factor
+//       disjoint factor
+//       weight factor
+
+//       generations penalization
+//       target number of species
+//       dynamic threshold step size
